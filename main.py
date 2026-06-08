7aa28741dcf3e64cc82037617a5933cfa45cac3f
EriclesM <ericlesmarcal@gmail.com>
2026-06-08 18:08:35 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Setup Start
+pygame.init()
+window = pygame.display.set_mode(size=(1024, 720))
+# Setup End
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # End pygame
+
+
